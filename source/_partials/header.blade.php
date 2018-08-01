2cd1f36b344f06154b351ae30fe1913077afd62e
Anthony Terell <anthonygterrell@gmail.com>
2018-08-01 14:06:17 -0500

    Update logo in header

--- source/_partials/header.blade.php
+++ source/_partials/header.blade.php
@@ -1,5 +1,5 @@
 <header class="px-4 bg-white border-b mb-8 flex shadow-md">
-    <section class="container flex mx-auto">
+    <section class="container flex mx-auto px-4">
         <div class="flex flex-1">
             <a href="{{ $page->url('/') }}" class="inline-flex items-center pt-4 pb-2 sm:py-6 font-bold">
                 @include('_partials.logo')
