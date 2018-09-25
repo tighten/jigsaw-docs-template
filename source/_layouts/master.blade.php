0445c659571a6a00db2b2a15e6b07e9d43c4e990
Anthony Terrell <anthonygterrell@gmail.com>
2018-09-25 16:56:12 -0400

    WIP-landing page

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -14,7 +14,7 @@
         <title>{{ $page->title ? "{$page->title} | " : "" }}{{ $page->docsTitle }}</title>
 
         <link rel="home" href="{{ $page->baseUrl }}">
-        <link rel="icon" href="{{ $page->url('/assets/img/logo.png') }}">
+        <link rel="icon" href="{{ $page->url('favicon.ico') }}">
 
         @stack('meta')
 
@@ -26,7 +26,22 @@
     </head>
     <body>
         <main id="vue-app" class="content">
-            @yield('navigation')
+            <header class="p-4 bg-white border-b mb-8 flex shadow">
+                <section class="container max-w-2xl flex mx-auto px-4">
+                    <div class="flex flex-1">
+                        <a href="{{ $page->url('/') }}" class="inline-flex items-center font-bold">
+                            <img src="/assets/img/logo.svg" alt="Logo" />
+                        </a>
+                    </div>
+
+                    <div class="flex flex-1 align-right justify-end items-center">
+                        <input id="docsearch" class="bg-grey-lighter outline-none px-4r py-2 rounded-full text-grey-darker search-field" type="text" name="docsearch" value="">
+
+                        <navigation-toggle></navigation-toggle>
+                    </div>
+                </section>
+            </header>
+
             @yield('body')
         </main>
 
--- source/index.blade.php
+++ source/index.blade.php
@@ -1,12 +1,45 @@
 @extends('_layouts.master')
 
 @section('body')
-    <div class="md:flex">
-        <div class="mb-8 lg:mb-0 px-4 w-full">
-            <div class="my-6 pb-6 border-b border-light-grey">
-                <p>Welcome to the Jigsaw docs.</p>
+<section class="container max-w-2xl py-16 mx-auto px-4">
+    <div class="flex flex-col lg:flex-row mb-6 md:mb-16">
+        <div>
+            <h1 class="font-normal tracking-normal leading-normal">
+                Welcome to Jigsaw Docs <br>
+                Static sites for modern developers
+            </h1>
+            <p class="w-3/4 text-lg">
+                Jigsaw is a framework for rapidly building static sites using the same modern tooling that powers your web applications.
+            </p>
+
+            <div class="flex mt-8">
+                <a href="/docs/installation" title="Get started in the docs" class="bg-blue py-1 px-6 text-white rounded mr-4">Get started</a>
+
+                <a href="/docs" title="Download Jigsaw" class="bg-grey-light py-1 px-6 text-blue rounded">Download</a>
             </div>
-            <p><a href="{{ $page->url('docs/installation') }}">Get started!</a></p>
+        </div>
+
+        <img src="/assets/img/logo-large.svg" alt="{{ $page->docsTitle }}" class="w-full w-1/2">
+    </div>
+
+    <div class="flex justify-between">
+        <div class="flex flex-col w-1/3">
+            <img src="/assets/img/icon-window.svg" class="h-8 w-8 mb-4">
+            <h3 class="font-normal tracking-normal leading-normal mb-2">Blade <br> Templating </h3>
+            <p class="text-sm leading-normal">Blade is a powerful, simple, and beautiful templating language, but until now it wasn't an option if you were building a simple static site that didn't need a complex PHP backend</p>
+        </div>
+
+        <div class="flex flex-col w-1/3">
+            <img src="/assets/img/icon-terminal.svg" class="h-8 w-8 mb-4">
+            <h3 class="font-normal tracking-normal leading-normal mb-2">Use Markdown for <br> content-driven pages</h3>
+            <p class="text-sm leading-normal">Markdown is a fantastic writing format for things like articles, blog posts, or documentation pages. Jigsaw makes it painless to create a layout in Blade, and fill it with content written in Markdown.</p>
+        </div>
+
+        <div class="flex flex-col w-1/3">
+            <img src="/assets/img/icon-stack.svg" class="h-8 w-8 mb-4">
+            <h3 class="font-normal tracking-normal leading-normal mb-2">Compile your assets using <br> Laravel Mix </h3>
+            <p class="text-sm leading-normal">Jigsaw sites are configured with support for Laravel Mix out of the box. If you've ever used Mix in a Laravel project, you already know how to use Mix with Jigsaw.</p>
         </div>
     </div>
+</section>
 @endsection
