c8c3f20530ef957cdd8374c7fb96557306f2de4d
Anthony Terrell <anthonygterrell@gmail.com>
2018-09-25 15:38:54 -0400

    Enable sourceMaps() to prevent Webpack sourceMap console bug.

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,7 @@ mix.webpackConfig({
 });
 
 mix.js('source/_assets/js/main.js', 'js')
+    .sourceMaps()
     .sass('source/_assets/sass/main.scss', 'css/main.css')
     .options({
         processCssUrls: false,
