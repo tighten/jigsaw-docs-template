e8256873b81250af3c6cd83b86ffc3898daa5e0d
Keith Damiani <keith@tighten.co>
2018-10-19 10:01:30 -0400

    Update config.php
    Co-Authored-By: anthonyterrell <anthonygterrell@gmail.com>

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 return [
     'siteName' => 'Jigsaw Docs',
-    'siteDescription' => 'Give your documentation a boost with Jigsaw. Generate elegant, static, docs quickly and easily.',
+    'siteDescription' => 'Give your documentation a boost with Jigsaw. Generate elegant, static docs quickly and easily.',
     'title' => 'Documentation powered by Jigsaw',
     'copyrightHolder' => 'Tighten',
     'docsearchApiKey' => '',
