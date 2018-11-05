0704493c54a1f351ac6b574fee40b9424d9f5f58
Keith Damiani <keith@tighten.co>
2018-11-05 13:24:56 -0500

    Use highlight.js and selectively import languages to reduce bundle size

--- source/_assets/js/main.js
+++ source/_assets/js/main.js
@@ -1,8 +1,19 @@
 window.docsearch = require('docsearch.js');
 window.Vue = require('vue');
-window.hljs = require('highlightjs');
 
 Vue.config.productionTip = false;
+
+import hljs from 'highlight.js/lib/highlight';
+
+hljs.registerLanguage('bash', require('highlight.js/lib/languages/bash'));
+hljs.registerLanguage('css', require('highlight.js/lib/languages/css'));
+hljs.registerLanguage('html', require('highlight.js/lib/languages/xml'));
+hljs.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'));
+hljs.registerLanguage('json', require('highlight.js/lib/languages/json'));
+hljs.registerLanguage('markdown', require('highlight.js/lib/languages/markdown'));
+hljs.registerLanguage('php', require('highlight.js/lib/languages/php'));
+hljs.registerLanguage('scss', require('highlight.js/lib/languages/scss'));
+hljs.registerLanguage('yaml', require('highlight.js/lib/languages/yaml'));
 
 import NavigationToggle from './components/navigation-toggle.vue';
 import Navigation from './components/navigation.vue';
