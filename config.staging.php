30e253eae5da9ebcf10eaaaa201ea88bbf3355ab
Keith Damiani <keith@tighten.co>
2018-12-07 11:30:36 -0500

    Add Docsearch credentials to staging

--- config.staging.php
+++ config.staging.php
@@ -3,4 +3,8 @@
 return [
     'baseUrl' => 'http://jigsaw-docs-staging.tighten.co',
     'production' => false,
+
+    // DocSearch credentials
+    'docsearchApiKey' => 'a4161e174ada160adbff1c329ca5b460',
+    'docsearchIndexName' => 'docs_skeleton',
 ];
