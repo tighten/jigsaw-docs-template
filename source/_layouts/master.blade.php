c92081b16052a4ef6c3bbd555e4fd259bd29358f
Anthony Terrell <anthonygterrell@gmail.com>
2018-12-11 13:17:26 -0500

    Update base styles and tailwind config from blog

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -46,15 +46,15 @@
                     <a href="/" title="{{ $page->siteName }} home" class="inline-flex items-center">
                         <img class="h-8 mr-3 md:h-10" src="/assets/img/logo.svg" alt="{{ $page->siteName }} logo" />
 
-                        <h1 class="hidden text-2xl my-0 text-blue-darker font-semibold md:block">{{ $page->siteName }}</h1>
+                        <h1 class="hidden md:block text-2xl text-blue-darker font-semibold my-0">{{ $page->siteName }}</h1>
                     </a>
                 </div>
 
                 <div class="flex-1 text-right md:pl-10">
-                @if ($page->docsearchApiKey && $page->docsearchIndexName)
-                    <input name="docsearch" type="text" id="docsearch" placeholder="Search"
-                        class="docsearch w-full sm:w-2/3 md:w-1/3 bg-grey-lighter outline-none px-4 py-2 rounded-full text-grey-darker border border-grey focus:border-blue-light transition-fast">
-                @endif
+                    @if ($page->docsearchApiKey && $page->docsearchIndexName)
+                        <input name="docsearch" type="text" id="docsearch" placeholder="Search"
+                            class="docsearch transition-fast w-full sm:w-2/3 md:w-1/3 bg-grey-lighter outline-none rounded-full text-grey-darker border border-grey focus:border-blue-light px-4 py-2">
+                    @endif
                 </div>
             </div>
 
@@ -64,6 +64,15 @@
         <main role="main" class="min-h-screen">
             @yield('body')
         </main>
+
+
+        <footer class="bg-white text-center py-4 mt-12" role="contentinfo">
+            <p class="text-xs">
+                &copy; <a href="https://tighten.co" title="Tighten website">Tighten</a> {{ date('Y') }}.
+                Built with <a href="http://jigsaw.tighten.co" title="Jigsaw by Tighten">Jigsaw</a>
+                and <a href="https://tailwindcss.com" title="Tailwind CSS, a utility-first CSS framework">Tailwind CSS</a>.
+            </p>
+        </footer>
 
         <script src="{{ mix('js/main.js', 'assets/build') }}"></script>
 
@@ -79,13 +88,5 @@
         @endif
 
         @stack('scripts')
-
-        <footer class="bg-white text-center py-4 mt-12" role="contentinfo">
-            <p class="text-xs">
-                &copy; <a href="https://tighten.co" title="Tighten website">Tighten</a> {{ date('Y') }}.
-                Built with <a href="http://jigsaw.tighten.co" title="Jigsaw by Tighten">Jigsaw</a>
-                and <a href="https://tailwindcss.com" title="Tailwind CSS, a utility-first CSS framework">Tailwind CSS</a>.
-            </p>
-        </footer>
     </body>
-</html>
+    </html>
