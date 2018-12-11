822fd1242911fbeae38d466aa397597980f0fcfa
Anthony Terrell <anthonygterrell@gmail.com>
2018-12-11 10:21:44 -0500

    Decrease content width in documentation layout

--- source/_layouts/documentation.blade.php
+++ source/_layouts/documentation.blade.php
@@ -5,13 +5,13 @@
 @endsection
 
 @section('body')
-<section class="container max-w-4xl mx-auto px-8 py-4">
+<section class="container max-w-4xl mx-auto px-4 md:px-8 py-4">
     <div class="flex flex-col lg:flex-row">
         <nav id="js-nav-menu" class="nav-menu hidden lg:block">
             @include('_nav.menu', ['items' => $page->navigation])
         </nav>
 
-        <div class="docSearch-content w-full pb-16 lg:w-3/4 lg:pl-4" v-pre>
+        <div class="w-full lg:w-3/5 break-words pb-16 lg:pl-4" v-pre>
             @yield('content')
         </div>
     </div>
