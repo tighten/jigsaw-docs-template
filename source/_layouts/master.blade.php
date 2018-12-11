6fa1c93bc70a99f4adfedbf430ea3ae0478382b8
Anthony Terrell <anthonygterrell@gmail.com>
2018-12-11 13:59:56 -0500

    Update footer navigation from blog

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -65,15 +65,6 @@
             @yield('body')
         </main>
 
-
-        <footer class="bg-white text-center py-4 mt-12" role="contentinfo">
-            <p class="text-xs">
-                &copy; <a href="https://tighten.co" title="Tighten website">Tighten</a> {{ date('Y') }}.
-                Built with <a href="http://jigsaw.tighten.co" title="Jigsaw by Tighten">Jigsaw</a>
-                and <a href="https://tailwindcss.com" title="Tailwind CSS, a utility-first CSS framework">Tailwind CSS</a>.
-            </p>
-        </footer>
-
         <script src="{{ mix('js/main.js', 'assets/build') }}"></script>
 
         @if ($page->docsearchApiKey && $page->docsearchIndexName)
@@ -88,5 +79,19 @@
         @endif
 
         @stack('scripts')
+
+        <footer class="bg-white text-center text-sm mt-12 py-4" role="contentinfo">
+            <nav>
+                <ul class="flex flex-col md:flex-row justify-center list-reset">
+                    <li class="md:mr-2">
+                        &copy; <a href="https://tighten.co" title="Tighten website">Tighten</a> {{ date('Y') }}.
+                    </li>
+                    <li>
+                        Built with <a href="http://jigsaw.tighten.co" title="Jigsaw by Tighten">Jigsaw</a>
+                        and <a href="https://tailwindcss.com" title="Tailwind CSS, a utility-first CSS framework">Tailwind CSS</a>.
+                    </li>
+                </ul>
+            </nav>
+        </footer>
     </body>
-    </html>
+</html>
