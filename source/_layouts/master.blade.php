c4d7744d82c3066ed30212576af569cffd5a2c36
Keith Damiani <keith@tighten.co>
2018-12-18 20:22:13 -0500

    Adjust footer margin, remove extra <nav> tag

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -68,19 +68,17 @@
 
         @stack('scripts')
 
-        <footer class="bg-white text-center text-sm mt-12 lg:mt-32 py-4" role="contentinfo">
-            <nav>
-                <ul class="flex flex-col md:flex-row justify-center list-reset">
-                    <li class="md:mr-2">
-                        &copy; <a href="https://tighten.co" title="Tighten website">Tighten</a> {{ date('Y') }}.
-                    </li>
+        <footer class="bg-white text-center text-sm mt-12 py-4" role="contentinfo">
+            <ul class="flex flex-col md:flex-row justify-center list-reset">
+                <li class="md:mr-2">
+                    &copy; <a href="https://tighten.co" title="Tighten website">Tighten</a> {{ date('Y') }}.
+                </li>
 
-                    <li>
-                        Built with <a href="http://jigsaw.tighten.co" title="Jigsaw by Tighten">Jigsaw</a>
-                        and <a href="https://tailwindcss.com" title="Tailwind CSS, a utility-first CSS framework">Tailwind CSS</a>.
-                    </li>
-                </ul>
-            </nav>
+                <li>
+                    Built with <a href="http://jigsaw.tighten.co" title="Jigsaw by Tighten">Jigsaw</a>
+                    and <a href="https://tailwindcss.com" title="Tailwind CSS, a utility-first CSS framework">Tailwind CSS</a>.
+                </li>
+            </ul>
         </footer>
     </body>
 </html>
