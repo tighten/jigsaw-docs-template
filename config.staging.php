5e90cde931685a27432f113b85bd6c9ebe2d1eb7
Keith Damiani <keith@tighten.co>
2018-12-18 20:11:34 -0500

    Update staging URL

--- config.staging.php
+++ config.staging.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'baseUrl' => 'https://staging.my-jigsaw-docs.com',
+    'baseUrl' => 'http://jigsaw-docs-staging.tighten.co',
     'production' => false,
 
     // DocSearch credentials
