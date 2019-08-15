045229fb7791f1198e45e5cfe47f1a96d5fa414e
Cameron Elliott <cameron-elliott@users.noreply.github.com>
2019-08-15 00:37:58 -0700

    remove list-reset from lists

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -69,7 +69,7 @@
         @stack('scripts')
 
         <footer class="bg-white text-center text-sm mt-12 py-4" role="contentinfo">
-            <ul class="flex flex-col md:flex-row justify-center list-reset">
+            <ul class="flex flex-col md:flex-row justify-center">
                 <li class="md:mr-2">
                     &copy; <a href="https://tighten.co" title="Tighten website">Tighten</a> {{ date('Y') }}.
                 </li>
--- source/_nav/menu-item.blade.php
+++ source/_nav/menu-item.blade.php
@@ -1,4 +1,4 @@
-<li class="list-reset pl-4">
+<li class="pl-4">
     @if ($url = is_string($item) ? $item : $item->url)
         {{-- Menu item with URL--}}
         <a href="{{ $page->url($url) }}"
