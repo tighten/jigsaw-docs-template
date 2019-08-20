1e422b1b08bc785ac44714faa948cfdd84a18885
Cameron Elliott <cameron-elliott@users.noreply.github.com>
2019-08-19 20:43:46 -0700

    added /bloglist is simple article list, was /blog in 'blog' template

---
+++ source/bloglist.blade.php
@@ -0,0 +1,55 @@
+---
+pagination:
+    collection: posts
+    perPage: 4
+---
+@extends('_layouts.master')
+
+@push('meta')
+    <meta property="og:title" content="{{ $page->siteName }} Blog" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="{{ $page->getUrl() }}"/>
+    <meta property="og:description" content="The list of blog posts for {{ $page->siteName }}" />
+@endpush
+
+@section('body')
+    <h1>Blog</h1>
+
+    <hr class="border-b my-6">
+
+    @foreach ($pagination->items as $post)
+        @include('_components.post-preview-inline')
+
+        @if ($post != $pagination->items->last())
+            <hr class="border-b my-6">
+        @endif
+    @endforeach
+
+    @if ($pagination->pages->count() > 1)
+        <nav class="flex text-base my-8">
+            @if ($previous = $pagination->previous)
+                <a
+                    href="{{ $previous }}"
+                    title="Previous Page"
+                    class="bg-gray-200 hover:bg-gray-400 rounded mr-3 px-5 py-3"
+                >&LeftArrow;</a>
+            @endif
+
+            @foreach ($pagination->pages as $pageNumber => $path)
+                <a
+                    href="{{ $path }}"
+                    title="Go to Page {{ $pageNumber }}"
+                    class="bg-gray-200 hover:bg-gray-400 text-blue-700 rounded mr-3 px-5 py-3 {{ $pagination->currentPage == $pageNumber ? 'text-blue-600' : '' }}"
+                >{{ $pageNumber }}</a>
+            @endforeach
+
+            @if ($next = $pagination->next)
+                <a
+                    href="{{ $next }}"
+                    title="Next Page"
+                    class="bg-gray-200 hover:bg-gray-400 rounded mr-3 px-5 py-3"
+                >&RightArrow;</a>
+            @endif
+        </nav>
+    @endif
+@stop
