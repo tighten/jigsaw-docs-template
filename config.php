122d99cf06805e29bc364f80082a47ead68d46db
Cameron Elliott <cameron-elliott@users.noreply.github.com>
2019-08-19 21:19:19 -0700

    Add in config helpers used in blog generation
    isActive helper was already used by docs
    collections, getDate, getExcerpt, are the specific helpers needed by the blog blades

--- config.php
+++ config.php
@@ -5,6 +5,7 @@ return [
     'production' => false,
     'siteName' => 'Docs Starter Template',
     'siteDescription' => 'Beautiful docs powered by Jigsaw',
+    'siteAuthor' => 'Author Name',
 
     // Algolia DocSearch credentials
     'docsearchApiKey' => '',
@@ -14,9 +15,12 @@ return [
     'navigation' => require_once('navigation.php'),
 
     // helpers
+
+    // blog and docs helper
     'isActive' => function ($page, $path) {
         return ends_with(trimPath($page->getPath()), trimPath($path));
     },
+    // docs helper
     'isActiveParent' => function ($page, $menuItem) {
         if (is_object($menuItem) && $menuItem->children) {
             return $menuItem->children->contains(function ($child) use ($page) {
@@ -24,7 +28,48 @@ return [
             });
         }
     },
+    // docs helper
     'url' => function ($page, $path) {
         return starts_with($path, 'http') ? $path : '/' . trimPath($path);
+    },
+
+    // blog helper: collections
+    'collections' => [
+        'posts' => [
+            'author' => 'Author Name', // Default author, if not provided in a post
+            'sort' => '-date',
+            'path' => 'blog/{filename}',
+        ],
+        'categories' => [
+            'path' => '/blog/categories/{filename}',
+            'posts' => function ($page, $allPosts) {
+                return $allPosts->filter(function ($post) use ($page) {
+                    return $post->categories ? in_array($page->getFilename(), $post->categories, true) : false;
+                });
+            },
+        ],
+    ],
+
+    // blog helper
+    'getDate' => function ($page) {
+        return Datetime::createFromFormat('U', $page->date);
+    },
+    // blog helper
+    'getExcerpt' => function ($page, $length = 255) {
+        $content = $page->excerpt ?? $page->getContent();
+        $cleaned = strip_tags(
+            preg_replace(['/<pre>[\w\W]*?<\/pre>/', '/<h\d>[\w\W]*?<\/h\d>/'], '', $content),
+            '<code>'
+        );
+
+        $truncated = substr($cleaned, 0, $length);
+
+        if (substr_count($truncated, '<code>') > substr_count($truncated, '</code>')) {
+            $truncated .= '</code>';
+        }
+
+        return strlen($cleaned) > $length
+            ? preg_replace('/\s+?(\S+)?$/', '', $truncated) . '...'
+            : $cleaned;
     },
 ];
