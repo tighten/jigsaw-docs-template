2e75542ba4025b04b616a5629a28d4b8cac23e08
Cameron Elliott <cameron-elliott@users.noreply.github.com>
2019-08-19 20:41:29 -0700

    Add about page, taken from blog template

---
+++ source/about.blade.php
@@ -0,0 +1,22 @@
+@extends('_layouts.master')
+
+@push('meta')
+    <meta property="og:title" content="About {{ $page->siteName }}" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="{{ $page->getUrl() }}"/>
+    <meta property="og:description" content="A little bit about {{ $page->siteName }}" />
+@endpush
+
+@section('body')
+    <h1>About</h1>
+
+    <img src="/assets/img/about.png"
+        alt="About image"
+        class="flex rounded-full h-64 w-64 bg-contain mx-auto md:float-right my-6 md:ml-10">
+
+    <p class="mb-6">This is where you can give a little more information about yourself or site. If you'd like to change the structure of this page, you can find the file at <code>source/about.blade.php</code></p>
+
+    <p class="mb-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum officia dolorem accusantium veniam quae, possimus, temporibus explicabo magni voluptas. fugit natus deserunt atque veniam possimus earum harum itaque est!</p>
+
+    <p class="mb-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum officia dolorem accusantium veniam quae, possimus, temporibus explicabo magni voluptas. fugit natus deserunt atque veniam!</p>
+@endsection
