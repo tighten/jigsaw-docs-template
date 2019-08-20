43b23d42ef18940fcb4b4f0769d4825ea769a13c
Cameron Elliott <cameron-elliott@users.noreply.github.com>
2019-08-19 19:22:18 -0700

    merge 'blog' master.blade into 'docs' for 'site'
    main changes:
     add atom href link to meta
    throw away the vue stuff for blog searching to be replaces with algolia

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -24,6 +24,7 @@
 
         <link rel="home" href="{{ $page->baseUrl }}">
         <link rel="icon" href="/favicon.ico">
+        <link href="/blog/feed.atom" type="application/atom+xml" rel="alternate" title="{{ $page->siteName }} Atom Feed">
 
         @stack('meta')
 
