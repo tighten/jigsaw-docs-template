d97fa92baa08b0d6949d41e735a2c07e36639147
Keith Damiani <keith@tighten.co>
2020-02-08 09:06:54 -0500

    Replace deprecated string helpers in config.php

--- config.php
+++ config.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Str;
+
 return [
     'baseUrl' => '',
     'production' => false,
@@ -15,7 +17,7 @@ return [
 
     // helpers
     'isActive' => function ($page, $path) {
-        return ends_with(trimPath($page->getPath()), trimPath($path));
+        return Str::endsWith(trimPath($page->getPath()), trimPath($path));
     },
     'isActiveParent' => function ($page, $menuItem) {
         if (is_object($menuItem) && $menuItem->children) {
@@ -25,6 +27,6 @@ return [
         }
     },
     'url' => function ($page, $path) {
-        return starts_with($path, 'http') ? $path : '/' . trimPath($path);
+        return Str::startsWith($path, 'http') ? $path : '/' . trimPath($path);
     },
 ];
