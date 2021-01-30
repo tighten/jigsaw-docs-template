a14559b13d3418530b6807385e1926d47d93b68d
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2021-01-29 23:41:56 -0500

    Use PostCSS instead of Sass

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,15 +5,14 @@ mix.disableSuccessNotifications();
 mix.setPublicPath('source/assets/build');
 
 mix.js('source/_assets/js/main.js', 'js')
-    .sass('source/_assets/sass/main.scss', 'css/main.css')
+    .css('source/_assets/css/main.css', 'css/main.css', [
+        require('postcss-import'),
+        require('tailwindcss'),
+        require('postcss-nesting'),
+    ])
     .jigsaw({
         watch: ['config.php', 'source/**/*.md', 'source/**/*.php', 'source/**/*.scss'],
     })
-    .options({
-        processCssUrls: false,
-        postCss: [
-            require('tailwindcss'),
-        ],
-    })
+    .options({ processCssUrls: false })
     .sourceMaps()
     .version();
