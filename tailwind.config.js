58334126c6c28ad9ae0ff79d9c2977ce5333bf25
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2021-01-29 22:51:32 -0500

    Move custom fontSize values directly into theme so they override Tailwind's

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,17 @@ module.exports = {
     },
   },
   theme: {
+    fontSize: {
+      'sm': '.925rem',
+      'base': '1rem',
+      'lg': '1.125rem',
+      'xl': '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.75rem',
+      '4xl': '2.125rem',
+      '5xl': '2.625rem',
+      '6xl': '10rem',
+    },
     extend: {
       fontFamily: {
         sans: [
@@ -41,18 +52,6 @@ module.exports = {
       boxShadow: {
         'lg': '0 -1px 27px 0 rgba(0, 0, 0, 0.04), 0 4px 15px 0 rgba(0, 0, 0, 0.08)',
       }
-    },
-    fontSize: {
-      'xs': '.8rem',
-      'sm': '.925rem',
-      'base': '1rem',
-      'lg': '1.125rem',
-      'xl': '1.25rem',
-      '2xl': '1.5rem',
-      '3xl': '1.75rem',
-      '4xl': '2.125rem',
-      '5xl': '2.625rem',
-      '6xl': '10rem',
     },
   },
   variants: {
