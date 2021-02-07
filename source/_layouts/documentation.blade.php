395488cbfa6c1120182bb9726a4b81812054731e
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2021-02-06 22:30:05 -0500

    Convert menu to use inline styles and render twice

--- source/_layouts/documentation.blade.php
+++ source/_layouts/documentation.blade.php
@@ -7,11 +7,14 @@
 @section('body')
 <section class="container max-w-8xl mx-auto px-6 md:px-8 py-4">
     <div class="flex flex-col lg:flex-row">
-        <nav id="js-nav-menu" class="nav-menu hidden lg:block">
+        <nav id="js-nav-menu" class="hidden lg:hidden w-auto -mt-12 -mx-8 mb-8 p-4 pt-8 bg-gray-200 border-b shadow">
+            @include('_nav.menu', ['items' => $page->navigation])
+        </nav>
+        <nav class="hidden lg:block w-1/4 mt-1 -mx-4 pr-4">
             @include('_nav.menu', ['items' => $page->navigation])
         </nav>
 
-        <div class="DocSearch-content w-full lg:w-3/5 break-words pb-16 lg:pl-4" v-pre>
+        <div class="DocSearch-content w-full lg:w-3/5 pb-16 lg:pl-4 break-words">
             @yield('content')
         </div>
     </div>
--- source/_nav/menu-item.blade.php
+++ source/_nav/menu-item.blade.php
@@ -1,15 +1,17 @@
 <li class="pl-4">
-    @if ($url = is_string($item) ? $item : $item->url)
-        {{-- Menu item with URL--}}
-        <a href="{{ $page->url($url) }}"
-            class="{{ 'lvl' . $level }} {{ $page->isActiveParent($item) ? 'lvl' . $level . '-active' : '' }} {{ $page->isActive($url) ? 'active font-semibold text-blue-500' : '' }} nav-menu__item hover:text-blue-500"
-        >
-            {{ $label }}
-        </a>
-    @else
-        {{-- Menu item without URL--}}
-        <p class="nav-menu__item text-gray-600">{{ $label }}</p>
-    @endif
+    <span class="block mb-3 mt-0 {{-- text-sm --}} no-underline">
+        @if ($url = is_string($item) ? $item : $item->url)
+            {{-- Menu item with URL--}}
+            <a href="{{ $page->url($url) }}"
+                class="{{ 'lvl' . $level }} {{ $page->isActiveParent($item) ? 'lvl' . $level . '-active' : '' }} {{ $page->isActive($url) ? 'active font-semibold text-blue-500' : 'text-gray-800' }} hover:text-blue-500"
+            >
+                {{ $label }}
+            </a>
+        @else
+            {{-- Menu item without URL--}}
+            <p class="text-gray-600">{{ $label }}</p>
+        @endif
+    </span>
 
     @if (! is_string($item) && $item->children)
         {{-- Recursively handle children --}}
