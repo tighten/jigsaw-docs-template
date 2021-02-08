5a8b7dd49d74d605b93294c61cdf6121b8090f27
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2021-02-07 20:42:10 -0500

    Slightly increase base font size

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -39,7 +39,7 @@
         @endif
     </head>
 
-    <body class="flex flex-col justify-between min-h-screen bg-gray-100 text-gray-800 leading-normal font-sans">
+    <body class="flex flex-col justify-between min-h-screen bg-gray-100 text-gray-800 text-lg leading-normal font-sans">
         <header class="flex items-center shadow bg-white border-b h-24 mb-8 py-4" role="banner">
             <div class="container flex items-center max-w-8xl mx-auto px-4 lg:px-8">
                 <div class="flex items-center">
